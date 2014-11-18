d13e6ea1668e4ca544b9b16513a221eb03286a62
Alex V. Surnin <info@volumnet.ru>
2014-11-18 19:22:40 +0500

    .

--- resources/cart.tmp.php
+++ resources/cart.tmp.php
@@ -38,7 +38,7 @@ function formatPrice($price)
                 <?php } ?>
               </td>
               <td><a <?php echo $row2->url ? 'href="' . htmlspecialchars($row2->url) . '"' : ''?>><?php echo htmlspecialchars($row->name)?></a></td>
-              <td data-role="price" data-price="<?php echo number_format($row->realprice, 2, '.', '')?>" class="span1">
+              <td data-role="price" data-price="<?php echo number_format($row->realprice, 2, '.', '')?>" class="span1" style="white-space: nowrap">
                 <?php echo formatPrice($row->realprice)?> р.
                 <?php if ($Cart->cartType->no_amount) { ?>
                     <input type="hidden" name="amount[<?php echo htmlspecialchars((int)$row->id . '_' . $row->meta)?>]" value="<?php echo (int)$row->amount?>" />
