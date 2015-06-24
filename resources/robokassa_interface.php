717f1ceba4990eaaf381125c4a01a9879cbc6aee
Alex V. Surnin <info@volumnet.ru>
2015-06-24 19:32:16 +0500

    .

--- resources/robokassa_interface.php
+++ resources/robokassa_interface.php
@@ -3,52 +3,57 @@ use \RAAS\CMS\Snippet;
 use \RAAS\Application;
 use \RAAS\CMS\Shop\Order;
 use \RAAS\CMS\Shop\Order_History;
-
-if ($_REQUEST['SignatureValue']) {
+if (in_array($_GET['action'], array('result', 'success', 'fail')) && $_REQUEST['InvId']) {
     // Подписанное значение - либо RESULT URL, либо SUCCESS URL, либо FAIL URL
     $inv_id = $_REQUEST['InvId'];
     $Item = new Order($inv_id);
-    $crc = strtoupper($_REQUEST['SignatureValue']);
-    if ($_GET['action'] == 'result') {
-        while (ob_get_level()) {
-            ob_end_clean();
-        }
-        $my_crc = strtoupper(md5($_REQUEST['OutSum'] . ':' . $inv_id . ':' . $Block->epay_pass2));
-        if ($my_crc != $crc) {
-            echo 'Invalid signature';
-        } elseif (!$Item->id) {
-            echo 'Invalid order ID#';
-        } else {
-            // Все ок
-            $history = new Order_History();
-            $history->uid = Application::i()->user->id;
-            $history->order_id = (int)$Item->id;
-            $history->status_id = (int)$Item->status_id;
-            $history->paid = 1;
-            $history->post_date = date('Y-m-d H:i:s');
-            $history->description = PAID_VIA_ROBOKASSA;
-            $history->commit();
-
-            $Item->paid = 1;
-            $Item->commit();
-            echo 'OK' . (int)$Item->id;
-        }
-        exit;
-    } elseif (($_GET['action'] == 'success') || ($_GET['action'] == 'fail')) {
-        $my_crc = strtoupper(md5($_REQUEST['OutSum'] . ':' . $inv_id . ':' . $Block->epay_pass1));
-        if ($Item->id) {
-            $OUT['epayWidget'] = Snippet::importByURN('robokassa');
-            $OUT['Item'] = $Item;
-            if ($crc == $my_crc) {
-                if ($_GET['action'] == 'success') {
-                    $OUT['success'][(int)$Block->id] = sprintf(ORDER_SUCCESSFULLY_PAID, $Item->id);
-                } elseif ($_GET['action'] == 'fail') {
-                    $OUT['localError'] = array('order' => sprintf(ORDER_HAS_NOT_BEEN_PAID, $Item->id));
-                }
-            } else {
-                $OUT['localError'] = array('crc' => INVALID_CRC);
+    $crc = isset($_REQUEST['SignatureValue']) ? strtoupper($_REQUEST['SignatureValue']) : null;
+    switch ($_GET['action']) {
+        case 'result':
+            while (ob_get_level()) {
+                ob_end_clean();
             }
-        }
+            $my_crc = strtoupper(md5($_REQUEST['OutSum'] . ':' . $inv_id . ':' . $Block->epay_pass2));
+            if ($my_crc != $crc) {
+                echo 'Invalid signature';
+            } elseif (!$Item->id) {
+                echo 'Invalid order ID#';
+            } else {
+                // Все ок
+                $history = new Order_History();
+                $history->uid = Application::i()->user->id;
+                $history->order_id = (int)$Item->id;
+                $history->status_id = (int)$Item->status_id;
+                $history->paid = 1;
+                $history->post_date = date('Y-m-d H:i:s');
+                $history->description = PAID_VIA_ROBOKASSA;
+                $history->commit();
+
+                $Item->paid = 1;
+                $Item->commit();
+                echo 'OK' . (int)$Item->id;
+            }
+            exit;
+            break;
+        case 'success':
+            $my_crc = strtoupper(md5($_REQUEST['OutSum'] . ':' . $inv_id . ':' . $Block->epay_pass1));
+            if ($Item->id) {
+                $OUT['epayWidget'] = Snippet::importByURN('robokassa');
+                $OUT['Item'] = $Item;
+                if ($crc == $my_crc) {
+                    $OUT['success'][(int)$Block->id] = sprintf(ORDER_SUCCESSFULLY_PAID, $Item->id);
+                } else {
+                    $OUT['localError'] = array('crc' => INVALID_CRC);
+                }
+            }
+            break;
+        case 'fail':
+            if ($Item->id) {
+                $OUT['epayWidget'] = Snippet::importByURN('robokassa');
+                $OUT['Item'] = $Item;
+                $OUT['localError'] = array('order' => sprintf(ORDER_HAS_NOT_BEEN_PAID, $Item->id));
+            }
+            break;
     }
 } elseif ($Item->id && $_POST['epay']) {
     $OUT['epayWidget'] = Snippet::importByURN('robokassa');
