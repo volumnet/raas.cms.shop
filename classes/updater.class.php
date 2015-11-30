cb07047d0b33c4c76dd4ce169cc341623058da43
Alex V. Surnin <info@volumnet.ru>
2015-11-30 22:58:43 +0500

    .

--- classes/updater.class.php
+++ classes/updater.class.php
@@ -46,11 +46,11 @@ class Updater extends \RAAS\Updater
 
     public function update20151129()
     {
-        if (in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_forms"))) {
+        if (in_array(\SOME\SOME::_dbprefix() . "cms_forms", $this->tables) && in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_forms"))) {
             $SQL_query = "UPDATE " . \SOME\SOME::_dbprefix() . "cms_forms SET urn = 'order' WHERE (urn = '') AND (name = 'Форма заказа' OR name = 'Order form')";
             $this->SQL->query($SQL_query);
         }
-        if (!in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_shop_priceloaders"))) {
+        if (in_array(\SOME\SOME::_dbprefix() . "cms_shop_priceloaders", $this->tables) && !in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_shop_priceloaders"))) {
             $SQL_query = "ALTER TABLE " . \SOME\SOME::_dbprefix() . "cms_shop_priceloaders 
                             ADD urn VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'URN' AFTER name,
                             ADD INDEX (urn)";
@@ -58,7 +58,7 @@ class Updater extends \RAAS\Updater
             $SQL_query = "UPDATE " . \SOME\SOME::_dbprefix() . "cms_shop_priceloaders SET urn = 'default' WHERE (urn = '') AND (name = 'Стандартный загрузчик прайсов' OR name = 'Default price loader')";
             $this->SQL->query($SQL_query);
         }
-        if (!in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_shop_imageloaders"))) {
+        if (in_array(\SOME\SOME::_dbprefix() . "cms_shop_imageloaders", $this->tables) && !in_array('urn', $this->columns(\SOME\SOME::_dbprefix() . "cms_shop_imageloaders"))) {
             $SQL_query = "ALTER TABLE " . \SOME\SOME::_dbprefix() . "cms_shop_imageloaders 
                             ADD urn VARCHAR(255) NOT NULL DEFAULT '' COMMENT 'URN' AFTER name,
                             ADD INDEX (urn)";
