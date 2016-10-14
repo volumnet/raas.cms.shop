6fc648bcc86e5b2aec246414ce9ddcbe2ddd9f72
Alex V. Surnin <info@volumnet.ru>
2016-10-14 14:21:15 +0500

    .

--- resources/catalog_filter.tmp.php
+++ resources/catalog_filter.tmp.php
@@ -85,6 +85,7 @@ $price2 = ceil(($DATA['price_to'] ?: $maxPrice) / (int)$priceStep) * (int)$price
             <div class="col-xs-1">до</div>
             <div class="col-xs-5"><input type="number" class="form-control" min="0" max="<?php echo (int)$maxPrice?>" step="<?php echo (int)$priceStep?>" name="price_to" placeholder="До" value="<?php echo htmlspecialchars($price2)?>"></div>
           </div>
+          <div class="catalog-filter__slider" id="catalog-filter__slider"></div>
         </div>
       </div>
       <div class="col-sm-3">
@@ -97,7 +98,7 @@ $price2 = ceil(($DATA['price_to'] ?: $maxPrice) / (int)$priceStep) * (int)$price
     </div>
     <div class="row">
       <div class="col-sm-3"></div>
-      <div class="col-sm-4"><div class="catalog-filter__slider" id="catalog-filter__slider"></div></div>
+      <div class="col-sm-4"></div>
       <div class="col-sm-1"></div>
     </div>
   </form>
