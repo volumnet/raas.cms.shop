24efd2d4943eb7a885c61c09bde1bd06a662f4fe
Alex V. Surnin <info@volumnet.ru>
2018-04-17 18:03:06 +0500

    .

--- resources/my_orders.tmp.php
+++ resources/my_orders.tmp.php
@@ -12,10 +12,10 @@ $getField = function($row) {
     $val = $row->doRich();
     switch ($row->datatype) {
         case 'date':
-            $arr[$key] = date(DATEFORMAT, strtotime($val));
+            $arr[$key] = date('d.m.Y', strtotime($val));
             break;
         case 'datetime-local':
-            $arr[$key] = date(DATETIMEFORMAT, strtotime($val));
+            $arr[$key] = date('d.m.Y H:i', strtotime($val));
             break;
         case 'color':
             $arr[$key] = '<span style="display: inline-block; height: 16px; width: 16px; background-color: ' . htmlspecialchars($val) . '"></span>';
@@ -58,7 +58,7 @@ $getField = function($row) {
         <div data-role="feedback-form">
           <div class="form-group">
             <label class="control-label col-sm-3 col-md-2" style="padding-top: 0"><?php echo STATUS?>:</label>
-            <div class="col-sm-9 col-md-4"><?php echo $Item->paid ? PAYMENT_PAID : PAYMENT_NOT_PAID?></div>
+            <div class="col-sm-9 col-md-4"><?php echo $Item->paid ? 'Оплачен' : 'Не оплачен'?></div>
           </div>
           <?php
           foreach ($Item->fields as $row) {
@@ -75,107 +75,116 @@ $getField = function($row) {
         </div>
       </div>
       <p>&nbsp;</p>
-      <table class="table table-striped cart-table" data-role="cart-table">
-        <tbody>
-          <?php $sum = $am = 0; foreach ($Item->items as $row) {?>
-            <tr data-role="cart-item">
-              <td class="cart-table__image-col">
-                <?php if ($row->visImages) { ?>
-                    <a <?php echo $row->url ? 'href="' . htmlspecialchars($row->url) . '"' : ''?>>
-                      <img src="/<?php echo htmlspecialchars(addslashes($row->visImages[0]->tnURL))?>" alt="<?php echo htmlspecialchars($row->visImages[0]->name ?: $row->name)?>" /></a>
-                <?php } ?>
-              </td>
-              <td class="cart-table__name-col">
-                <h3><a <?php echo $row->url ? 'href="' . htmlspecialchars($row->url) . '"' : ''?>><?php echo htmlspecialchars($row->name)?></a></h3>
-                <?php if ($Cart->cartType->no_amount) { ?>
-                    <input type="hidden" name="amount[<?php echo htmlspecialchars((int)$row->id . '_' . $row->meta)?>]" value="<?php echo (int)$row->amount?>" />
-                <?php } else { ?>
-                    <p><?php echo AMOUNT . ': ' . (int)$row->amount?></p>
-                    <?php
-                }
-                foreach ($row->fields as $f) {
-                    if ($val = array_filter(array_map(array($f, 'doRich'), $f->getValues(true)))) {
-                        ?>
-                        <p>
-                          <?php echo htmlspecialchars($f->name)?>:
-                          <?php
-                          if (in_array($key, array('metal'))) {
-                              echo htmlspecialchars(mb_strtolower(implode(', ', $val)));
-                          } else {
-                              echo htmlspecialchars(implode(', ', $val));
-                          }
-                          ?>
-                        </p>
-                        <?php
-                    }
-                }
-                ?>
-              </td>
-              <td class="cart-table__sum-col"><span data-role="sum"><?php echo formatPrice($row->amount * $row->realprice)?></span> <span class="fa fa-rub"></span></td>
-            </tr>
-          <?php $sum += $row->amount * $row->realprice; $am += $row->amount; } ?>
-          <tr>
-            <th class="cart-table__image-col"></th>
-            <th class="cart-table__name-col"><?php echo TOTAL_SUM?>:</th>
-            <th class="cart-table__sum-col"><span data-role="total-sum"><?php echo formatPrice($sum)?></span>&nbsp;<span class="fa fa-rub"></span></th>
-          </tr>
-        </tbody>
-      </table>
-
-  <?php } elseif ($Set) { ?>
-      <table class="table table-striped my-orders__table">
-        <tbody>
-          <?php foreach ($Set as $row) { ?>
+      <div class="cart">
+        <div class="cart__inner">
+          <table class="table table-striped cart-table" data-role="cart-table">
+            <thead>
               <tr>
-                <td><a href="?id=<?php echo (int)$row->id?>"><?php echo Lang::i()->_('ORDER_NUMBER', $Page)?> <?php echo (int)$row->id?></td>
-                <td><a href="?id=<?php echo (int)$row->id?>"><?php echo date(Lang::i()->_('DATETIME_FORMAT', $Page), strtotime($row->post_date))?></a></td>
-                <td>
-                  <a href="?id=<?php echo (int)$row->id?>">
-                  <?php
-                  $temp = array();
-                  foreach ($row->items as $row2) {
-                      $arr = $row2->name;
-                      if ($row2->amount > 1) {
-                          $arr .= ' - ' . (int)$row2->amount . 'x' . formatPrice($row2->realprice) . '<span class="fa fa-rub"></span> = ' . formatPrice($row2->amount * $row2->realprice) . ' <span class="fa fa-rub"></span>';
-                      } else {
-                          $arr .= ' = ' . formatPrice($row2->realprice) . ' <span class="fa fa-rub"></span>';
-                      }
-                      $temp[] = $arr;
-                  }
-                  echo implode('<br />', $temp);
-                  ?>
-                  </a>
-                </td>
-                <td>
-                  <?php
-                  $temp = array();
-                  if ($row->status->id) {
-                      $temp[] = $row->status->name;
-                  } else {
-                      // $temp[] = ORDER_STATUS_NEW;
-                  }
-                  if ($row->paid) {
-                      $temp[] = PAYMENT_PAID;
-                  } else {
-                      $temp[] = PAYMENT_NOT_PAID;
-                  }
-                  echo implode('<br />', $temp);
-                  ?>
-                </td>
-                <td class="my-orders__sum">
-                  <a href="?id=<?php echo (int)$row->id?>"><?php echo formatPrice($row->sum)?> <span class="fa fa-rub"></span></a>
-                </td>
-                <td>
-                  <?php if (!$row->status_id && !$row->paid && !$row->vis) { ?>
-                      <a href="#" data-id="<?php echo (int)$row->id?>" data-back="true" data-toggle="modal" data-target="#confirmDeleteOrderModal"><span class="fa fa-close"></span></a>
-                  <?php } ?>
-                </td>
+                <th class="cart-table__image-col"><?php echo IMAGE?></th>
+                <th class="cart-table__article-col"><?php echo ARTICLE?></th>
+                <th class="cart-table__name-col"><?php echo NAME?></th>
+                <th class="cart-table__price-col"><?php echo PRICE?></th>
+                <th class="cart-table__amount-col"><?php echo AMOUNT?></th>
+                <th class="cart-table__sum-col"><?php echo SUM?></th>
               </tr>
-          <?php } ?>
-        </tbody>
-      </table>
+            </thead>
+            <tbody data-role="cart__body_main">
+              <?php $sum = $am = 0; foreach ($Item->items as $row) {?>
+                  <tr>
+                    <td class="cart-table__image-col">
+                      <?php if ($row->visImages) { ?>
+                          <a <?php echo $row->url ? 'href="' . htmlspecialchars($row->url) . '"' : ''?>>
+                            <img src="/<?php echo htmlspecialchars(addslashes($row->visImages[0]->smallURL))?>" style="max-width: 48px" alt="<?php echo htmlspecialchars($row->visImages[0]->name ?: $row->name)?>" /></a>
+                      <?php } ?>
+                    </td>
+                    <td class="cart-table__article-col"><a <?php echo $row->url ? 'href="' . htmlspecialchars($row->url) . '"' : ''?>><?php echo htmlspecialchars($row->article)?></a></td>
+                    <td class="cart-table__name-col"><a <?php echo $row->url ? 'href="' . htmlspecialchars($row->url) . '"' : ''?>><?php echo htmlspecialchars($row->name)?></a></td>
+                    <td data-role="price" class="cart-table__price-col">
+                      <?php echo formatPrice($row->realprice)?> <span class="fa fa-rub"></span>
+                    </td>
+                    <td class="cart-table__amount-col"><?php echo (int)$row->amount?></td>
+                    <td class="cart-table__sum-col"><?php echo formatPrice($row->amount * $row->realprice)?> <span class="fa fa-rub"></span></td>
+                  </tr>
+              <?php $sum += $row->amount * $row->realprice; $am += $row->amount; } ?>
+            </tbody>
+            <tbody>
+              <tr>
+                <th colspan="<?php echo !$Cart->cartType->no_amount ? '4' : '3'?>"><?php echo TOTAL_SUM?>:</th>
+                <th class="cart-table__amount-col"><span data-role="total-amount"><?php echo (int)$am; ?></span></td>
+                <th class="cart-table__sum-col"><span data-role="total-sum"><?php echo formatPrice($sum)?></span>&nbsp;<span class="fa fa-rub"></span></th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+  <?php } elseif ($Set) { ?>
+      <div class="cart">
+        <div class="cart__inner">
+          <table class="table table-striped cart-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Дата</th>
+                <th>Товары</th>
+                <th>Статус</th>
+                <th>Сумма</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php foreach ($Set as $row) { ?>
+                  <tr>
+                    <td><a href="?id=<?php echo (int)$row->id?>"><?php echo (int)$row->id?></a></td>
+                    <td><a href="?id=<?php echo (int)$row->id?>"><?php echo date('d.m.Y', strtotime($row->post_date))?></a></td>
+                    <td>
+                      <a href="?id=<?php echo (int)$row->id?>">
+                      <?php
+                      $temp = array();
+                      foreach ($row->items as $row2) {
+                          $arr = $row2->name;
+                          if ($row2->amount > 1) {
+                              $arr .= ' - ' . (int)$row2->amount . 'x' . formatPrice($row2->realprice) . '<span class="fa fa-rub"></span> = ' . formatPrice($row2->amount * $row2->realprice) . ' <span class="fa fa-rub"></span>';
+                          } else {
+                              $arr .= ' = ' . formatPrice($row2->realprice) . ' <span class="fa fa-rub"></span>';
+                          }
+                          $temp[] = $arr;
+                      }
+                      echo implode('<br />', $temp);
+                      ?>
+                      </a>
+                    </td>
+                    <td>
+                      <?php
+                      $temp = array();
+                      if ($row->status->id) {
+                          $temp[] = $row->status->name;
+                      } else {
+                          // $temp[] = ORDER_STATUS_NEW;
+                      }
+                      if ($row->paid) {
+                          $temp[] = 'Оплачен';
+                      } else {
+                          $temp[] = 'Не оплачен';
+                      }
+                      echo implode('<br />', $temp);
+                      ?>
+                    </td>
+                    <td class="my-orders__sum">
+                      <a href="?id=<?php echo (int)$row->id?>"><?php echo formatPrice($row->sum)?> <span class="fa fa-rub"></span></a>
+                    </td>
+                    <td>
+                      <?php if (!$row->status_id && !$row->paid && !$row->vis) { ?>
+                          <a href="#" data-id="<?php echo (int)$row->id?>" data-back="true" data-toggle="modal" data-target="#confirmDeleteOrderModal"><span class="fa fa-close"></span></a>
+                      <?php } ?>
+                    </td>
+                  </tr>
+              <?php } ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
   <?php } else { ?>
-    <p><?php echo Lang::i()->_('YOU_HAVE_NO_ORDERS_YET', $Page)?></p>
+    <p>У вас пока нет ни одного заказа</p>
   <?php } ?>
 </div>
 
@@ -184,7 +193,7 @@ $getField = function($row) {
     <div class="modal-content">
       <div class="modal-header" style="border-bottom: none">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title"><?php echo Lang::i()->_('ARE_YOU_SURE_TO_DELETE_THIS_ORDER', $Page)?></h4>
+        <h4 class="modal-title">Вы действительно хотите удалить заказ?</h4>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo CANCEL?></button>
