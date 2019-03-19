1c239ef260d5e170c47bca49b191101dc86c13a0
Alex V. Surnin <info@volumnet.ru>
2019-03-19 16:32:52 +0500

    .

---
+++ resources/interfaces/sberbank_interface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Интерфейс Сбербанка
+ * @param Block_Cart $Block Текущий блок
+ * @param Page $Page Текушая страница
+ * @param Order $Item текущий заказ
+ */
+namespace RAAS\CMS\Shop;
+
+use RAAS\CMS\Page;
+
+$interface = new SberbankInterface(
+    $Block,
+    $Page,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_SESSION,
+    $_SERVER,
+    $_FILES
+);
+$sberbankOut = $interface->process($Item);
+$OUT = array_merge($OUT, $sberbankOut);
