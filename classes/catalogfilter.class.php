f16569f9f478c77dece0a4c5a775c10453c2d719
Alex V. Surnin <info@volumnet.ru>
2019-08-13 15:54:50 +0500

    .

--- classes/catalogfilter.class.php
+++ classes/catalogfilter.class.php
@@ -819,7 +819,7 @@ class CatalogFilter
     /**
      * Получает переменные окружения по фильтру
      * @param array<
-     *            string[] имя свойства => array<mixed>|
+     *            string[] ID# свойства => array<mixed>|
      *                                     ['from' => float, 'to' => float]|
      *                                     ['like' => string] значение или набор значений
      *        > $filter Фильтр для проверки
@@ -853,7 +853,7 @@ class CatalogFilter
     /**
      * Получает канонический URL из фильтра
      * @param array<
-     *            string[] имя свойства => array<mixed>|
+     *            string[] ID# свойства => array<mixed>|
      *                                     ['from' => float, 'to' => float]|
      *                                     ['like' => string] значение или набор значений
      *        > $filter Фильтр для проверки
