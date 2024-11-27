4b7a5f5c31e88aebffaa469dae4f58ddf26c6050
Alex V. Surnin <info@volumnet.ru>
2024-11-27 12:52:20 +0500

    4.4.6 Пошаговая загрузка прайсов

--- classes/controllers/sub_priceloaders.class.php
+++ classes/controllers/sub_priceloaders.class.php
@@ -24,102 +24,23 @@ class Sub_Priceloaders extends \RAAS\Abstract_Sub_Controller
 
     public function run()
     {
-        if (in_array($this->action, [
-            'vis_unaffected_material',
-            'invis_unaffected_material',
-            'delete_unaffected_material',
-            'vis_unaffected_page',
-            'invis_unaffected_page',
-            'delete_unaffected_page',
-        ])) {
-            $loader = new PriceLoader($this->nav['pid'] ?? 0);
-            if (!$loader->id) {
-                exit;
-            }
-
-            $interfaceClassname = $loader->interface_classname;
-            $interface = new $interfaceClassname($loader);
-            $datafile = $this->getStepDataFilename($loader);
-            $data = $interface->loadStepData($datafile);
-            $ids = (array)($_GET['id'] ?? []);
-            $items = [];
-            $affectedIds = [];
-            $hashTag = '';
-            if (stristr($this->action, 'material')) {
-                $affectedIds = $interface->getStepAffectedMaterialsIds($data);
-                $classname = Material::class;
-                $hashTag = '#materials';
-                if (in_array('all', $ids)) {
-                    $allLoaderPagesIds = PageRecursiveCache::i()->getSelfAndChildrenIds((int)$data['rootCategoryId']);
-                    $allLoaderMaterialTypesIds = MaterialTypeRecursiveCache::i()->getSelfAndChildrenIds($loader->mtype);
-                    $sqlQuery = "SELECT DISTINCT tM.id
-                                   FROM " . Material::_tablename() . " AS tM
-                                   JOIN cms_materials_pages_assoc AS tMPA ON tMPA.id = tM.id
-                                  WHERE tM.pid IN (" . implode(", ", $allLoaderMaterialTypesIds) . ")
-                                    AND tMPA.pid IN (" . implode(", ", $allLoaderPagesIds) . ")";
-                    $ids = Material::_SQL()->getcol($sqlQuery);
-                }
-            } elseif (stristr($this->action, 'page')) {
-                $affectedIds = $interface->getStepAffectedPagesIds($data);
-                $classname = Page::class;
-                $hashTag = '#pages';
-            }
-            if (in_array('all', $ids)) {
-                $ids = [];
-                if (stristr($this->action, 'material')) {
-                    // @todo по всем материалам
-                } elseif (stristr($this->action, 'page')) {
-                    // @todo по всем страницам
-                }
-            }
-            $ids = array_map('intval', $ids);
-            $ids = array_values(array_diff($ids, $affectedIds));
-            if ($ids) {
-                $items = $classname::getSet([
-                    'where' => ["id IN (" . implode(", ", $ids) . ")"],
-                    'orderBy' => 'id',
-                ]);
-            }
-            $f = null;
-            foreach (['vis', 'invis', 'delete'] as $actionName) {
-                if (preg_match('/^' . $actionName . '_/umis', $this->action)) {
-                    $f = $actionName;
-                    break;
-                }
-            }
-            if ($f) {
-                if (isset($_GET['back'])) {
-                    $returnUrl = 'history:back';
-                } else {
-                    $returnUrl = $this->url . '&action=unaffected&id=' . (int)$loader->id;
-                }
-                $returnUrl .= $hashTag;
-                // var_dump($f, $items); exit;
-                StdSub::$f($items, $returnUrl, false);
-            }
-            exit;
-        } else {
-            $loader = new PriceLoader($this->id);
-            if ($loader->id) {
-                if ($this->action == 'download') {
-                    $this->download($loader);
-                } elseif ($this->action == 'unaffected') {
-                    $this->unaffected($loader);
-                } else {
-                    if ((
-                        ($loader->interface_classname == PriceloaderInterface::class) ||
-                        is_subclass_of($loader->interface_classname, PriceloaderInterface::class)
-                    ) && $loader->step_interface) {
-                        $this->stepUpload($loader);
-                    } else {
-                        $this->upload($loader);
-                    }
-                }
+        $loader = new PriceLoader($this->id);
+        if ($loader->id) {
+            if ($this->action == 'download') {
+                $this->download($loader);
             } else {
-                $this->view->showlist(['Set' => PriceLoader::getSet()]);
+                if ((
+                    ($loader->interface_classname == PriceloaderInterface::class) ||
+                    is_subclass_of($loader->interface_classname, PriceloaderInterface::class)
+                ) && $loader->step_interface) {
+                    $this->stepUpload($loader);
+                } else {
+                    $this->upload($loader);
+                }
             }
+        } else {
+            $this->view->showlist(['Set' => PriceLoader::getSet()]);
         }
-
     }
 
 
@@ -254,26 +175,6 @@ class Sub_Priceloaders extends \RAAS\Abstract_Sub_Controller
             case 2:
                 break;
             case 3:
-                $affectedMaterialsIds = $interface->getStepAffectedMaterialsIds($data);
-                $affectedPagesIds = $interface->getStepAffectedPagesIds($data);
-
-                $allLoaderPagesIds = PageRecursiveCache::i()->getSelfAndChildrenIds((int)$data['rootCategoryId']);
-                $allLoaderMaterialTypesIds = MaterialTypeRecursiveCache::i()->getSelfAndChildrenIds($loader->mtype);
-
-                $loaderArr['unaffectedMaterialsCount'] = 0;
-                if ($allLoaderPagesIds && $allLoaderMaterialTypesIds) {
-                    $sqlQuery = "SELECT COUNT(DISTINCT tM.id)
-                                   FROM " . Material::_tablename() . " AS tM
-                                   JOIN cms_materials_pages_assoc AS tMPA ON tMPA.id = tM.id
-                                  WHERE tM.pid IN (" . implode(", ", $allLoaderMaterialTypesIds) . ")
-                                    AND tMPA.pid IN (" . implode(", ", $allLoaderPagesIds) . ")";
-                    if ($affectedMaterialsIds) {
-                        $sqlQuery .= " AND tM.id NOT IN (" . implode(", ", $affectedMaterialsIds) . ")";
-                    }
-                    $sqlResult = Material::_SQL()->getvalue($sqlQuery);
-                    $loaderArr['unaffectedMaterialsCount'] = (int)$sqlResult;
-                }
-                $loaderArr['unaffectedPagesCount'] = count(array_diff($allLoaderPagesIds, $affectedPagesIds));
                 break;
         }
 
@@ -324,173 +225,6 @@ class Sub_Priceloaders extends \RAAS\Abstract_Sub_Controller
         $out = $form->process();
         $out['loader'] = $loader;
         $this->view->download($out);
-    }
-
-
-    /**
-     * Обработка незадействованных материалов и страниц
-     * @param PriceLoader $loader Загрузчик
-     */
-    public function unaffected(PriceLoader $loader)
-    {
-        $interfaceClassname = $loader->interface_classname;
-        $interface = new $interfaceClassname($loader);
-        $datafile = $this->getStepDataFilename($loader);
-        $data = $interface->loadStepData($datafile);
-        $affectedMaterialsIds = $interface->getStepAffectedMaterialsIds($data);
-        $affectedPagesIds = $interface->getStepAffectedPagesIds($data);
-        $materialType = $loader->Material_Type;
-        $materialTypesIds = MaterialTypeRecursiveCache::i()->getSelfAndChildrenIds($loader->mtype);
-        $allLoaderPagesIds = PageRecursiveCache::i()->getSelfAndChildrenIds((int)$data['rootCategoryId']);
-
-
-        $out = [];
-        $localError = [];
-        if (($data['step'] ?? 0) < 3) {
-            $localError[] = [
-                'name' => 'INVALID',
-                'value' => 'data',
-                'description' => $this->view->_('YOU_CAN_VIEW_UNAFFECTED_ONLY_AFTER_PRICE_APPLYING'),
-            ];
-        }
-
-        // Материалы
-        $searchString = $this->nav['search_string'] ?? '';
-        $pageNum = $this->nav['page'] ?? 1;
-        $sort = $this->nav['sort'] ?? 'post_date';
-        $order = $this->nav['order'] ?? 'asc';
-        $columns = array_filter(
-            $materialType->fields,
-            function ($x) {
-                return $x->show_in_table;
-            }
-        );
-
-        $sqlQuery = "SELECT SQL_CALC_FOUND_ROWS tM.* ";
-        if (!$materialType->global_type) {
-            $sqlQuery .= ", (
-                                SELECT COUNT(tMPA2.pid)
-                                  FROM " . Material::_dbprefix() . "cms_materials_pages_assoc AS tMPA2
-                                 WHERE id = tM.id
-                            ) AS pages_counter";
-        }
-
-        $sqlQuery .= " FROM " . Material::_tablename() . " AS tM ";
-        // 2016-01-14, AVS: добавил поиск по данным
-        $sqlQuery .= " WHERE tM.pid IN (" . implode(", ", $materialTypesIds) . ")";
-        if ($affectedMaterialsIds) {
-            $sqlQuery .= " AND tM.id NOT IN (" . implode(", ", $affectedMaterialsIds) . ")";
-        }
-        if (!$materialType->global_type) {
-            $sqlQuery .= " AND (
-                                SELECT COUNT(*)
-                                  FROM " . Material::_dbprefix() . "cms_materials_pages_assoc AS tMPA
-                                 WHERE tMPA.id = tM.id
-                                   AND tMPA.pid IN (" . implode(", ", $allLoaderPagesIds) . ")
-                            )";
-        }
-        // 2016-01-14, AVS: добавил поиск по данным
-        if ($searchString) {
-            $likeSearchString = Application::i()->SQL->real_escape_string($searchString);
-            $sqlQuery .= "  AND (
-                                    tM.id = '" . $likeSearchString . "'
-                                 OR tM.name LIKE '%" . $likeSearchString . "%'
-                                 OR tM.description LIKE '%" . $likeSearchString . "%'
-                                 OR tM.urn LIKE '%" . $likeSearchString . "%'
-                                 OR (
-                                        SELECT COUNT(*)
-                                          FROM " . Material::_dbprefix() . Material_Field::DATA_TABLE . " AS tD
-                                         WHERE tD.pid = tM.id
-                                           AND tD.value LIKE '%" . $likeSearchString . "%'
-                                )
-                            )";
-        }
-        $sqlQuery .= " GROUP BY tM.id
-                       ORDER BY NOT tM.priority ASC, tM.priority ASC";
-        $pages = new Pages($pageNum, Application::i()->registryGet('rowsPerPage'));
-        if (isset($sort, $columns[$sort]) && ($row = $columns[$sort])) {
-            $reverse = (isset($order) && ($order == 'desc'));
-            $_order = $reverse ? 'desc' : 'asc';
-            $ids = Package::i()->getCompareSQL($row, $reverse);
-            if ($ids) {
-                $sqlQuery .= ", FIELD(tM.id, " . implode(", ", $ids) . ")";
-            }
-        } else {
-            switch ($sort) {
-                case 'name':
-                case 'urn':
-                case 'modify_date':
-                    $_sort = 'tM.' . $sort;
-                    break;
-                default:
-                    $sort = 'post_date';
-                    $_sort = 'tM.post_date';
-                    break;
-            }
-            if (isset($order) && ($order == 'desc')) {
-                $_order = 'desc';
-            } elseif (!isset($order) && in_array($sort, ['post_date', 'modify_date'])) {
-                $_order = 'desc';
-            } else {
-                $_order = 'asc';
-            }
-            $sqlQuery .= ", " . $_sort . " " . strtoupper($_order);
-        }
-        $Set = Material::getSQLSet($sqlQuery, $pages);
-        $out['Set'] = $Set;
-        $out['Pages'] = $pages;
-        $out['sort'] = $sort;
-        $out['order'] = $_order;
-        $out['mtype'] = $materialType;
-        $out['searchString'] = $searchString;
-        $out['pagesSource'] = $this->getUnaffectedPagesSource((int)$data['rootCategoryId'], $affectedPagesIds);
-
-        $out['localError'] = $localError;
-        $out['loader'] = $loader;
-        $out['step'] = ($data['step'] ?? '');
-        $this->view->unaffected($out);
-    }
-
-
-    /**
-     * Получает список незадействованных категорий для дерева
-     * @param int $catId ID# категории, для которой определяем дочерние
-     * @param int[] $affectedPagesIds ID# задействованных категорий
-     * @return array <pre><code>array<[
-     *     'value' => int ID# категории,
-     *     'text' => string Название категории,
-     *     'children' =>? array дочерние элементы (рекурсивно)
-     * ]></code></pre>
-     */
-    public function getUnaffectedPagesSource($catId = 0, array $affectedPagesIds = [], $level = 0): array
-    {
-        $result = [];
-        $children = PageRecursiveCache::i()->getChildrenCache($catId);
-        foreach ($children as $child) {
-            $resultEntry = [
-                'value' => (int)$child['id'],
-                'caption' => $child['name'],
-                'name' => $child['name'],
-                'affected' => in_array((int)$child['id'], $affectedPagesIds),
-            ];
-            if ($ch = $this->getUnaffectedPagesSource((int)$child['id'], $affectedPagesIds, $level + 1)) {
-                $resultEntry['children'] = $ch;
-            }
-            if (!$resultEntry['affected'] || $ch) {
-                $result[] = $resultEntry;
-            }
-        }
-        if (!$level) {
-            $rootCache = PageRecursiveCache::i()->cache[$catId];
-            $result = [[
-                'value' => $catId,
-                'caption' => $rootCache['name'],
-                'name' => $rootCache['name'],
-                'affected' => in_array((int)$child['id'], $affectedPagesIds),
-                'children' => $result,
-            ]];
-        }
-        return $result;
     }
 
 
